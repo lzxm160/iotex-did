d5a1b6abedffaaf98b4b6fffff65c7256f92e354
lzxm160 <lzxm160@sina.com>
2020-05-28 14:31:07 +0800

    x

--- smartcontracts/contracts/IoTeXDIDProxy.sol
+++ smartcontracts/contracts/IoTeXDIDProxy.sol
@@ -51,15 +51,27 @@ contract IoTeXDIDProxy is IoTeXDIDStorage,Ownable {
         require(_impl != address(0), "implementation not set");
 
         assembly {
-            let ptr := mload(0x40)
+            ptr := mload(0x40)
             calldatacopy(ptr, 0, calldatasize)
-            let result := delegatecall(gas, _impl, ptr, calldatasize, 0, 0)
-            let size := returndatasize
+            result := delegatecall(gas, _impl, ptr, calldatasize, 0, 0)
+            size := returndatasize
             returndatacopy(ptr, 0, size);
 
             switch result
             case 0 { revert(ptr, size); }
             default { return(ptr, size); }
+
+//            // retrieve the size of the code, this needs assembly
+//            let size := extcodesize(_addr)
+//            // allocate output byte array - this could also be done without assembly
+//            // by using o_code = new bytes(size)
+//            o_code := mload(0x40)
+//            // new "memory end" including padding
+//            mstore(0x40, add(o_code, and(add(add(size, 0x20), 0x1f), not(0x1f))))
+//            // store length in memory
+//            mstore(o_code, size)
+//            // actually retrieve the code, this needs assembly
+//            extcodecopy(_addr, add(o_code, 0x20), 0, size)
         }
     }
 }
