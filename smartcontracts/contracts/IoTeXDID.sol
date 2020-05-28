dd7396f415a15b48d8e4789b82a391de74182b01
lzxm160 <lzxm160@sina.com>
2020-05-28 13:49:39 +0800

    x

--- smartcontracts/contracts/IoTeXDID.sol
+++ smartcontracts/contracts/IoTeXDID.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.24 <=0.6.1;
 import "./IoTeXDIDStorage.sol";
 
 contract IoTeXDID is IoTeXDIDStorage{
-    modifier onlyDIDOwner(string didInput) {
+    modifier onlyDIDOwner(string memory didInput) {
         string memory didString = generateDIDString();
         if (bytes(didInput).length > 0) {
             require(compareStrings(didInput, didString), "caller does not own the given did");
@@ -17,7 +17,7 @@ contract IoTeXDID is IoTeXDIDStorage{
     event UpdateURI(string indexed didString, string uri);
     event DeleteDID(string indexed didString);
 
-    function createDID(string id, bytes32 hash, string uri) public {
+    function createDID(string memory id, bytes32 memory hash, string memory uri) public {
         if (bytes(id).length > 0) {
             require(compareStrings(id, addrToString(msg.sender)), "id does not match creator");
         }
@@ -28,28 +28,28 @@ contract IoTeXDID is IoTeXDIDStorage{
         emit CreateDID(toLower(addrToString(msg.sender)), resultDID);
     }
 
-    function updateHash(string did, bytes32 hash) public onlyDIDOwner(did) {
+    function updateHash(string memory did, bytes32 memory hash) public onlyDIDOwner(did) {
         dids[generateDIDString()].hash = hash;
         emit UpdateHash(generateDIDString(), hash);
     }
 
-    function updateURI(string did, string uri) public onlyDIDOwner(did) {
+    function updateURI(string memory did, string memory uri) public onlyDIDOwner(did) {
         dids[generateDIDString()].uri = uri;
         emit UpdateURI(generateDIDString(), uri);
     }
 
-    function deleteDID(string did) public onlyDIDOwner(did) {
+    function deleteDID(string memory did) public onlyDIDOwner(did) {
         dids[generateDIDString()].exist = false;
         emit DeleteDID(generateDIDString());
     }
 
-    function getHash(string did) public view returns (bytes32 memory) {
+    function getHash(string memory did) public view returns (bytes32 memory) {
         string memory didString = toLower(did);
         require(dids[didString].exist, "did does not exist");
         return dids[didString].hash;
     }
 
-    function getURI(string did) public view returns (string memory) {
+    function getURI(string memory did) public view returns (string memory) {
         string memory didString = toLower(did);
         require(dids[didString].exist, "did does not exist");
         return dids[didString].uri;
