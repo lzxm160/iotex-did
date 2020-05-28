94c953015c667e189ec86bcced3eaa404c474b2c
lzxm160 <lzxm160@sina.com>
2020-05-28 13:46:21 +0800

    x

--- smartcontracts/contracts/IoTeXDIDStorage.sol
+++ smartcontracts/contracts/IoTeXDIDStorage.sol
@@ -9,11 +9,11 @@ contract IoTeXDIDStorage {
     }
     mapping(string => DID) public dids;
 
-    function generateDIDString() private view returns (string) {
+    function generateDIDString() private view returns (string memory) {
         return string(abi.encodePacked(didPrefix, addrToString(msg.sender)));
     }
 
-    function addrToString(address _addr) internal pure returns(string) {
+    function addrToString(address _addr) internal pure returns(string memory) {
         bytes32 value = bytes32(uint256(_addr));
         bytes memory alphabet = "0123456789abcdef";
 
@@ -27,11 +27,11 @@ contract IoTeXDIDStorage {
         return string(str);
     }
 
-    function compareStrings (string a, string b) internal pure returns (bool) {
+    function compareStrings (string a, string b) internal pure returns (bool memory) {
         return (keccak256(abi.encodePacked((toLower(a)))) == keccak256(abi.encodePacked((toLower(b)))));
     }
 
-    function toLower(string str) internal pure returns (string) {
+    function toLower(string str) internal pure returns (string memory) {
 		bytes memory bStr = bytes(str);
 		bytes memory bLower = new bytes(bStr.length);
 		for (uint i = 0; i < bStr.length; i++) {
