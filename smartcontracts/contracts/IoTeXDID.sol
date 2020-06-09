b076543b2c1031477ecced5d452d1c14172a3e1f
lzxm160 <lzxm160@sina.com>
2020-06-09 21:24:01 +0800

    x

--- smartcontracts/contracts/IoTeXDID.sol
+++ smartcontracts/contracts/IoTeXDID.sol
@@ -90,32 +90,18 @@ contract IoTeXDID is IoTeXDIDStorage{
         return dids[didString].uri;
     }
 
-    function strConcat(string _a, string _b, string _c, string _d, string _e) internal returns (string){
+    function strConcat(string memory _a, string memory _b, string memory _c, string memory _d) internal returns (string memory){
         bytes memory _ba = bytes(_a);
         bytes memory _bb = bytes(_b);
         bytes memory _bc = bytes(_c);
         bytes memory _bd = bytes(_d);
-        bytes memory _be = bytes(_e);
-        string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
-        bytes memory babcde = bytes(abcde);
+        string memory abcd = new string(_ba.length + _bb.length + _bc.length + _bd.length);
+        bytes memory badcd = bytes(abcd);
         uint k = 0;
-        for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
-        for (i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
-        for (i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
-        for (i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
-        for (i = 0; i < _be.length; i++) babcde[k++] = _be[i];
-        return string(babcde);
-    }
-
-    function strConcat(string _a, string _b, string _c, string _d) internal returns (string) {
-        return strConcat(_a, _b, _c, _d, "");
-    }
-
-    function strConcat(string _a, string _b, string _c) internal returns (string) {
-        return strConcat(_a, _b, _c, "", "");
-    }
-
-    function strConcat(string _a, string _b) internal returns (string) {
-        return strConcat(_a, _b, "", "", "");
+        for (uint i = 0; i < _ba.length; i++) babcd[k++] = _ba[i];
+        for (i = 0; i < _bb.length; i++) babcd[k++] = _bb[i];
+        for (i = 0; i < _bc.length; i++) babcd[k++] = _bc[i];
+        for (i = 0; i < _bd.length; i++) babcd[k++] = _bd[i];
+        return string(badcd);
     }
 }
