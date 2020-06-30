6853db8cf60a1d46411a7bcc940cd1b18998c847
lzxm160 <lzxm160@sina.com>
2020-06-30 18:46:15 +0800

    x

--- smartcontracts/test/iotexdid.test.js
+++ smartcontracts/test/iotexdid.test.js
@@ -1,21 +1,45 @@
-const IoTeXDID = artifacts.require('AddressBasedDIDManagerWithAgentEnabled.sol');
+const IoTeXDID = artifacts.require(
+  "AddressBasedDIDManagerWithAgentEnabled.sol"
+);
 
-contract('AddressBasedDIDManagerWithAgentEnabled', function(accounts) {
-    beforeEach(async function() {
-        this.contract = await IoTeXDID.new();
+contract("AddressBasedDIDManagerWithAgentEnabled", function (accounts) {
+  beforeEach(async function () {
+    this.contract = await IoTeXDID.new();
+  });
+  describe("create did", function () {
+    it("success", async function () {
+      let hash = web3.utils.fromAscii("hash");
+      let uri = "s3://iotex-did/documents";
+      let did = "did:io:" + accounts[1].toLowerCase();
+      let msg =
+        "I authorize," +
+        accounts[0].toLowerCase() +
+        " to create DID " +
+        did +
+        " in contract with " +
+        this.contract +
+        " (" +
+        h +
+        ", " +
+        uri +
+        ")";
+      // I authorize ", addrToString(agent), " to create DID ", did, " in contract with ", addrToString(address(this)), " (", h, ", ", uri, ")"
+      let prefix = "\x19Ethereum Signed Message:\\n" + msg.length;
+      console.log(prefix + msg);
+      let sig = await web3.eth.accounts.sign(
+        prefix + msg,
+        accounts[1].privateKey
+      );
+
+      sig = sig.signature;
+      console.log(sig);
+
+      await this.contract.registerByAgent(
+        hash,
+        uri,
+        accounts[1].toLowerCase(),
+        sig
+      );
     });
-    describe("create did", function () {
-        it('success', async function() {
-            let hash = web3.utils.fromAscii("hash");
-            let uri = "s3://iotex-did/documents";
-            let msg = 'I authorize' + accounts[0].toLowerCase() + ' to register ' + UUID;
-            console.log(msg);
-            let sig = await web3.eth.accounts.sign(msg, '0x1cd94a139f784fea91aee5a77b2519ab5852348a4525df12db2ef002d922e1e7');
-
-            sig = sig.signature;
-            console.log(sig);
-
-            await this.contract.createDID(UUID, sig, hash, uri, {from: accounts[0]});
-        })
-    })
-})
+  });
+});
