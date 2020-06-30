1101de5b9ae74a7fe466c29a724a4878e9f66e90
lzxm160 <lzxm160@sina.com>
2020-06-30 17:36:29 +0800

    x

--- smartcontracts/migrations/1_initial_migration.js
+++ smartcontracts/migrations/1_initial_migration.js
@@ -7,6 +7,14 @@ const CloudServiceAddr = "0x627306090abab3a6e1400e9345bc60c78a8bef57";
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
   // deployer.deploy(DecentralizedIdentifier);
-  deployer.deploy(IoTeXDID,"did:io:",0x0);
+  String.prototype.getBytes = function () {
+    var bytes = [];
+    for (var i = 0; i < this.length; ++i) {
+      bytes.push(this.charCodeAt(i));
+    }
+    return bytes;
+  };
+  var str = "did:io:";
+  deployer.deploy(IoTeXDID,str.getBytes(),0x0);
   // deployer.deploy(MockDeviceDID, CloudServiceAddr);
 };
