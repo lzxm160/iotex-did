ac41f41a8f1c783471f7ce22f57e1b7e1c256ee1
lzxm160 <lzxm160@sina.com>
2020-07-15 11:56:03 +0800

    x

--- smartcontracts/test/iotexdid.test.js
+++ smartcontracts/test/iotexdid.test.js
@@ -21,6 +21,8 @@ contract("UCamDIDManager", function (accounts) {
         "this.contract.toLowerCase()",
         this.contract.address.toLowerCase()
       );
+      console.log("accounts[0].toLowerCase()", accounts[0].toLowerCase());
+      console.log("accounts[1].toLowerCase()", accounts[1].toLowerCase());
       let uri = "s3://iotex-did/documents";
       let did = "did:io:ucam:" + accounts[1].toLowerCase();
       // "I authorize ", addrToString(agent), " to create DID ", did, " in contract with ", addrToString(address(this)), " (", h, ", ", uri, ")"
@@ -36,40 +38,24 @@ contract("UCamDIDManager", function (accounts) {
         ", " +
         uri +
         ")";
-      let prefix = "\x19Ethereum Signed Message:\n" + msg.length;
-      console.log("prefix + msg", prefix + msg);
 
-      // let accounts = await web3.eth.getAccounts();
-      // let msgHash1 = web3.utils.sha3(prefix + msg);
-
+      console.log("msg", msg);
       let sig = await web3.eth.sign(msg, accounts[1]);
       console.log("sig", sig);
-      // let privateKey =
-      //   "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
-      //
-      // let sigObj = await web3.eth.accounts.sign(msg, privateKey);
-      // let msgHash2 = sigObj.messageHash;
-      //
-      // let sig2 = sigObj.signature;
-      //
-      // let sig = await web3.eth.accounts.sign(
-      //   prefix + msg,
-      //   accounts[1].privateKey
-      // );
 
-      // let sigs = sig.signature;
-      // console.log("sigs", sigs);
-      console.log("accounts[0].toLowerCase()", accounts[0].toLowerCase());
-      console.log("accounts[1].toLowerCase()", accounts[1].toLowerCase());
-
-      console.log("sig", sig.slice(2, sig.byteLength));
       await this.contract.createDIDByAgent(
-        accounts[1].toLowerCase(),
+        web3.utils.hexToBytes(accounts[1].toLowerCase()),
         web3.utils.hexToBytes(testHash),
         uri.getBytes(),
-        accounts[0].toLowerCase(),
+        web3.utils.hexToBytes(accounts[0].toLowerCase()),
         web3.utils.hexToBytes(sig)
       );
     });
   });
 });
+// console.log("sig", sig.slice(2, sig.byteLength));
+// let prefix = "\x19Ethereum Signed Message:\n" + msg.length;
+// console.log("prefix + msg", prefix + msg);
+
+// let accounts = await web3.eth.getAccounts();
+// let msgHash1 = web3.utils.sha3(prefix + msg);
