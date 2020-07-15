a059135c141d4fe0a0734ce180288b5f59544e70
lzxm160 <lzxm160@sina.com>
2020-07-15 18:59:10 +0800

    x

--- smartcontracts/contracts/UCamDIDManager.sol
+++ smartcontracts/contracts/UCamDIDManager.sol
@@ -62,15 +62,13 @@ contract UCamDIDManager is Agentable, DIDManagerBase {
     }
 
     function bytes20ToString(bytes20 _addr) internal pure returns(string memory) {
-        bytes32 value = bytes32(uint256(_addr));
         bytes memory alphabet = "0123456789abcdef";
-
         bytes memory str = new bytes(42);
         str[0] = '0';
         str[1] = 'x';
         for (uint i = 0; i < 20; i++) {
-            str[2+i*2] = alphabet[uint8(value[i + 12] >> 4)];
-            str[3+i*2] = alphabet[uint8(value[i + 12] & 0x0f)];
+            str[2+i*2] = alphabet[uint8(_addr[i] >> 4)];
+            str[3+i*2] = alphabet[uint8(_addr[i] & 0x0f)];
         }
         return string(str);
     }
