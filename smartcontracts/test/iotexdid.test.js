a4aa0f5d8b32e5aa88684543e717caa375f8beee
lzxm160 <lzxm160@sina.com>
2020-07-16 11:27:17 +0800

    x

--- smartcontracts/test/iotexdid.test.js
+++ smartcontracts/test/iotexdid.test.js
@@ -68,6 +68,7 @@ contract("UCamDIDManager", function (accounts) {
         ")";
       console.log(web3.utils.hexToBytes(testHash));
       console.log("msg", msg);
+      console.log("uri", web3.utils.hexToBytes(web3.utils.asciiToHex(uri)));
       let sig = await web3.eth.sign(msg, accounts[1]);
       console.log("sig", sig);
       console.log();
