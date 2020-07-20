51ff899f718c1c713fc9c82ca4e1ca4a494c4915
lzxm160 <lzxm160@sina.com>
2020-07-20 14:36:52 +0800

    x

--- smartcontracts/contracts/UCamDIDManager.sol
+++ smartcontracts/contracts/UCamDIDManager.sol
@@ -18,19 +18,9 @@ contract UCamDIDManager is Agentable, DIDManagerBase {
         require(domainID.length == 20, "invalid DID");
 
         uint160 uid = 0;
-        uint8 b1 = 0;
-        uint8 b2 = 0;
-        for (uint i = 0; i < 20; i += 2){
+        for (uint i = 0; i < 20; i++){
             uid *= 256;
-            b1 = uint8(domainID[i]);
-            b2 = uint8(domainID[i+1]);
-            if ((b1 >= 97)&&(b1 <= 102)) b1 -= 87;
-            else if ((b1 >= 48)&&(b1 <= 57)) b1 -= 48;
-            else if ((b1 >= 65)&&(b1 <= 70)) b1 -= 55;
-            if ((b2 >= 97)&&(b2 <= 102)) b2 -= 87;
-            else if ((b2 >= 48)&&(b2 <= 57)) b2 -= 48;
-            else if ((b2 >= 65)&&(b2 <= 70)) b2 -= 55;
-            uid += (b1*16+b2);
+            uid += uint8(domainID[i]);
         }
         return bytes20(uid);
 //                uint160 m = 0;
